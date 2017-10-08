8e5d41e190cfbb93169d97b1f9e331128a6ee17b
Isaac Kwan <g@isaac.pw>
2017-10-08 09:48:04 +0800

    progress

--- py3/lab3.py
+++ py3/lab3.py
@@ -1,6 +1,37 @@
 from browser import document, alert, window
+from unittest import TestCase, main
 
-def echo(ev):
-	alert(document["zone"].value)
+class Lab3Test(TestCase):
+	def test_upper(self):
+		self.assertEqual('foo'.upper(), 'FOO')
+		self.assertEqual('foo'.upper(), 'FO2O')
 
-document['mybutton'].bind('click',echo)
+	def test_isupper(self):
+		self.assertTrue('FOO'.isupper())
+		self.assertFalse('Foo'.isupper())
+
+class Lab3:
+	def __init__(self):
+		super().__init__()
+
+	def upload(self, name, content):
+		exec(content)
+
+	def finishedUpload(self):
+		alert('finished upload')
+		result = Lab3Test()
+		window.console.log(result)
+		main()
+		print(result)
+		alert('run')
+
+lab3 = Lab3()
+
+def handleUpload(event):
+	lab3.upload(event.detail.name, event.detail.body)
+
+def handleFinishedUpload(event):
+	lab3.finishedUpload()
+
+document['drop_zone'].bind('upload', handleUpload)
+document['drop_zone'].bind('finishedUpload', handleFinishedUpload)
